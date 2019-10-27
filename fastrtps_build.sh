5fc34a841ce5e0268af9f4531aad80f43d29d307
Dmitriy Borovikov <dmitry@borovikov.name>
2019-10-28 00:28:20 +0300

    Clone rtfs repos in script

--- fastrtps_build.sh
+++ fastrtps_build.sh
@@ -4,13 +4,24 @@ set -e
 echo "$1" # Build type
 echo MACOSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET"
 if [ ! -f "Framework/libfastrtps.1.dylib" ]; then
+if [ ! -d memory ]; then
+git clone --recurse-submodules https://github.com/foonathan/memory.git
+fi
 rm -rf $PROJECT_TEMP_DIR/memory
 mkdir -p $PROJECT_TEMP_DIR/memory || true
-cmake -Sfoonathan_memory_vendor -B$PROJECT_TEMP_DIR/memory -DCMAKE_INSTALL_PREFIX=build/osx -DCMAKE_BUILD_TYPE="$1"
+cmake -Smemory -B$PROJECT_TEMP_DIR/memory -DCMAKE_INSTALL_PREFIX=build/osx \
+-DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
+-DFOONATHAN_MEMORY_BUILD_TESTS=OFF \
+-DFOONATHAN_MEMORY_BUILD_TOOLS=ON \
+-DCMAKE_BUILD_TYPE="$1"
 cmake --build $PROJECT_TEMP_DIR/memory --target install
 rm -rf $PROJECT_TEMP_DIR/Fast-RTPS
+if [ ! -d Fast-RTPS ]; then
+git clone --recurse-submodules https://github.com/DimaRU/Fast-RTPS.git
+fi
 mkdir -p $PROJECT_TEMP_DIR/Fast-RTPS || true
-cmake -SFast-RTPS -B$PROJECT_TEMP_DIR/Fast-RTPS -Dfoonathan_memory_DIR=build/osx/share/foonathan_memory/cmake -DCMAKE_INSTALL_PREFIX=build/osx -DTHIRDPARTY=ON -DCMAKE_BUILD_TYPE="$1"
+cmake -SFast-RTPS -B$PROJECT_TEMP_DIR/Fast-RTPS -DCMAKE_INSTALL_PREFIX=build/osx \
+-Dfoonathan_memory_DIR=build/osx/share/foonathan_memory/cmake -DTHIRDPARTY=ON -DCMAKE_BUILD_TYPE="$1"
 cmake --build $PROJECT_TEMP_DIR/Fast-RTPS --target install
 mkdir Framework || true
 cp build/osx/lib/libfastrtps.1.dylib Framework/
