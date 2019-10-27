999f957838f825c89854ba8aab80efa6ab252ec1
Dmitriy Borovikov <dmitry@borovikov.name>
2019-10-27 14:04:39 +0300

    Add submodule deps

--- fastrtps_build.sh
+++ fastrtps_build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e
 export MACOSX_DEPLOYMENT_TARGET=10.14
 #git submodule update --init --recursive
-mkdir temp && cd temp
-cmake -DCMAKE_INSTALL_PREFIX=../build -DCMAKE_BUILD_TYPE=Release ../foonathan_memory_vendor
-cmake --build . --target install
-cd .. && rm -rf temp
-mkdir temp && cd temp
-cmake -Dfoonathan_memory_DIR=../build/share/foonathan_memory/cmake -DCMAKE_INSTALL_PREFIX=../build -DTHIRDPARTY=ON -INTERNALDEBUG=ON -DCMAKE_BUILD_TYPE="$1" ../Fast-RTPS
-cmake --build . --target install
-cd .. && rm -rf temp
+echo "$1" # Build type
+rm -rf $PROJECT_TEMP_DIR/memory
+mkdir -p $PROJECT_TEMP_DIR/memory || true
+cmake -Sfoonathan_memory_vendor -B$PROJECT_TEMP_DIR/memory -DCMAKE_INSTALL_PREFIX=build/osx -DCMAKE_BUILD_TYPE="$1"
+cmake --build $PROJECT_TEMP_DIR/memory --target install
+rm -rf $PROJECT_TEMP_DIR/Fast-RTPS
+mkdir -p $PROJECT_TEMP_DIR/Fast-RTPS || true
+cmake -SFast-RTPS -B$PROJECT_TEMP_DIR/Fast-RTPS -Dfoonathan_memory_DIR=build/osx/share/foonathan_memory/cmake -DCMAKE_INSTALL_PREFIX=build/osx -DTHIRDPARTY=ON -DCMAKE_BUILD_TYPE="$1"
+cmake --build $PROJECT_TEMP_DIR/Fast-RTPS --target install
 mkdir Framework || true
-cp build/lib/libfastrtps.1.dylib Framework/
-cp build/lib/libfastcdr.1.dylib Framework/
+cp build/osx/lib/libfastrtps.1.dylib Framework/
+cp build/osx/lib/libfastcdr.1.dylib Framework/
