49cad71ed5a4f7f9b1365396667139667449d190
Dmitriy Borovikov <dmitry@borovikov.name>
2019-11-13 00:37:14 +0300

    Improve iOS build: Use standard polly version. Use static libs.

--- fastrtps_build_ios.sh
+++ fastrtps_build_ios.sh
@@ -9,22 +9,23 @@ echo "$1" # Build type
 export POLLY_IOS_DEVELOPMENT_TEAM=$DEVELOPMENT_TEAM
 
 if ! which polly > /dev/null; then
-  echo "error: polly is not installed. Vistit https://github.com/DimaRU/polly.git to learn more."
+  echo "error: polly is not installed. Vistit https://github.com/ruslo/polly to learn more."
   exit 1
 fi
 
-if [ ! -f "build/ios/lib/libfastrtps.1.dylib" ]; then
+if [ ! -f "build/ios/lib/libfastrtps.a" ]; then
 if [ ! -d memory ]; then
 git clone --quiet --recurse-submodules -b ios $Foonathan_memory_repo memory
 fi
 rm -rf "$PROJECT_TEMP_DIR/memory"
 mkdir -p "$PROJECT_TEMP_DIR/memory" || true
-polly.py --toolchain ios-13-1-dep-12-1-x86-64-arm64 \
---verbose --install --ios-combined --ios-multiarch \
+polly.py --toolchain ios \
+--install --ios-combined --ios-multiarch \
 --config Release \
 --home memory \
 --output "$PROJECT_TEMP_DIR/memory" \
 --fwd CMAKE_CONFIGURATION_TYPES=Release \
+IOS_DEPLOYMENT_SDK_VERSION=$IPHONEOS_DEPLOYMENT_TARGET \
 CMAKE_INSTALL_PREFIX=build/ios \
 FOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
 FOONATHAN_MEMORY_BUILD_TESTS=OFF \
@@ -35,13 +36,14 @@ if [ ! -d Fast-RTPS ]; then
 git clone --quiet --recurse-submodules $FastRTPS_repo Fast-RTPS
 fi
 mkdir -p "$PROJECT_TEMP_DIR/Fast-RTPS" || true
-polly.py --toolchain ios-13-1-dep-12-1-x86-64-arm64 \
---verbose --install --ios-combined --ios-multiarch \
+polly.py --toolchain ios \
+--install --ios-combined --ios-multiarch \
 --config $1 \
 --home Fast-RTPS \
 --output "$PROJECT_TEMP_DIR/Fast-RTPS" \
 --fwd CMAKE_CONFIGURATION_TYPES=$1 \
-BUILD_SHARED_LIBS=YES \
+IOS_DEPLOYMENT_SDK_VERSION=$IPHONEOS_DEPLOYMENT_TARGET \
+BUILD_SHARED_LIBS=NO \
 EXPORT_FILE=NO \
 CMAKE_INSTALL_PREFIX=build/ios \
 foonathan_memory_DIR=build/ios/foonathan_memory/cmake \
