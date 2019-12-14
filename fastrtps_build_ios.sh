b81db13e46580404a17d4b792d67905a1f765c9a
Dmitriy Borovikov <dmitry@borovikov.name>
2019-12-14 23:25:17 +0300

    New fastrtps build system

--- fastrtps_build_ios.sh
+++ fastrtps_build_ios.sh
@@ -3,17 +3,23 @@
 # fastrtps_build_ios.sh
 # Copyright © 2019 Dmitriy Borovikov. All rights reserved.
 #
-export PATH=$HOME/Developer/tools/polly/bin:$PATH
 set -e
-echo "$1" # Build type
-export POLLY_IOS_DEVELOPMENT_TEAM=$DEVELOPMENT_TEAM
+echo "$CONFIGURATION" # Build type
 
+if [ -f "$BUILT_PRODUCTS_DIR/fastrtps/lib/libfastrtps.a" ]; then
+echo Already build "$BUILT_PRODUCTS_DIR/fastrtps/lib/libfastrtps.a"
+exit 0
+fi
+
+export POLLY_IOS_DEVELOPMENT_TEAM=$DEVELOPMENT_TEAM
+export CMAKE_BUILD_PARALLEL_LEVEL=2
+
+export PATH=$HOME/Developer/tools/polly/bin:$PATH
 if ! which polly > /dev/null; then
   echo "error: polly is not installed. Vistit https://github.com/ruslo/polly to learn more."
   exit 1
 fi
 
-if [ ! -f "build/ios/lib/libfastrtps.a" ]; then
 if [ ! -d memory ]; then
 git clone --quiet --recurse-submodules --depth 1 -b ios $Foonathan_memory_repo memory
 fi
@@ -26,7 +32,7 @@ polly.py --toolchain ios \
 --output "$PROJECT_TEMP_DIR/memory" \
 --fwd CMAKE_CONFIGURATION_TYPES=Release \
 IOS_DEPLOYMENT_SDK_VERSION=$IPHONEOS_DEPLOYMENT_TARGET \
-CMAKE_INSTALL_PREFIX=build/ios \
+CMAKE_INSTALL_PREFIX=$BUILT_PRODUCTS_DIR/fastrtps \
 FOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
 FOONATHAN_MEMORY_BUILD_TESTS=OFF \
 FOONATHAN_MEMORY_BUILD_TOOLS=OFF
@@ -38,17 +44,15 @@ fi
 mkdir -p "$PROJECT_TEMP_DIR/Fast-RTPS" || true
 polly.py --toolchain ios \
 --install --ios-combined --ios-multiarch \
---config $1 \
+--config $CONFIGURATION \
 --home Fast-RTPS \
 --output "$PROJECT_TEMP_DIR/Fast-RTPS" \
---fwd CMAKE_CONFIGURATION_TYPES=$1 \
+--fwd CMAKE_CONFIGURATION_TYPES=$CONFIGURATION \
+CMAKE_DEBUG_POSTFIX="" \
 IOS_DEPLOYMENT_SDK_VERSION=$IPHONEOS_DEPLOYMENT_TARGET \
 BUILD_SHARED_LIBS=NO \
-EXPORT_FILE=NO \
-CMAKE_INSTALL_PREFIX=build/ios \
-foonathan_memory_DIR=build/ios/foonathan_memory/cmake \
+CMAKE_INSTALL_PREFIX=$BUILT_PRODUCTS_DIR/fastrtps \
+foonathan_memory_DIR=$BUILT_PRODUCTS_DIR/fastrtps/foonathan_memory/cmake \
 THIRDPARTY=ON \
-BUILD_JAVA=OFF \
 COMPILE_EXAMPLES=OFF \
 BUILD_DOCUMENTATION=OFF
-fi
--- fastrtps_build_osx.sh
+++ fastrtps_build_osx.sh
@@ -4,15 +4,21 @@
 # Copyright © 2019 Dmitriy Borovikov. All rights reserved.
 #
 set -e
-set -x
-echo "$1" # Build type
-if [ ! -f "build/osx/lib/libfastrtps.a" ]; then
+
+echo $CONFIGURATION # Build type
+if [ -f "$BUILT_PRODUCTS_DIR/fastrtps/lib/libfastrtps.a" ]; then
+echo Already build $BUILT_PRODUCTS_DIR/fastrtps/lib/libfastrtps.a
+exit 0
+fi
+
+export CMAKE_BUILD_PARALLEL_LEVEL=2
+
 if [ ! -d memory ]; then
 git clone --quiet --recurse-submodules --depth 1 -b ios $Foonathan_memory_repo memory
 fi
 rm -rf "$PROJECT_TEMP_DIR/memory"
 mkdir -p "$PROJECT_TEMP_DIR/memory" || true
-cmake -Smemory -B"$PROJECT_TEMP_DIR/memory" -DCMAKE_INSTALL_PREFIX=build/osx \
+cmake -Smemory -B"$PROJECT_TEMP_DIR/memory" -DCMAKE_INSTALL_PREFIX=$BUILT_PRODUCTS_DIR/fastrtps \
 -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
 -DFOONATHAN_MEMORY_BUILD_TESTS=OFF \
 -DFOONATHAN_MEMORY_BUILD_TOOLS=ON \
@@ -25,10 +31,9 @@ git clone --quiet --recurse-submodules --depth 1 $FastRTPS_repo Fast-RTPS
 fi
 mkdir -p "$PROJECT_TEMP_DIR/Fast-RTPS" || true
 cmake -SFast-RTPS -B"$PROJECT_TEMP_DIR/Fast-RTPS" \
--DCMAKE_INSTALL_PREFIX=build/osx \
--Dfoonathan_memory_DIR=build/osx/share/foonathan_memory/cmake \
+-DCMAKE_INSTALL_PREFIX=$BUILT_PRODUCTS_DIR/fastrtps \
+-Dfoonathan_memory_DIR=$BUILT_PRODUCTS_DIR/fastrtps/share/foonathan_memory/cmake \
 -DTHIRDPARTY=ON \
 -DBUILD_SHARED_LIBS=OFF \
--DCMAKE_BUILD_TYPE=Debug
+-DCMAKE_BUILD_TYPE=$CONFIGURATION
 cmake --build "$PROJECT_TEMP_DIR/Fast-RTPS" --target install
-fi
