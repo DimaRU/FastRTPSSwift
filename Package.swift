c3f40e9b108b21be0404af6706110d484c7f34d4
Dmitriy Borovikov <dmitry@borovikov.name>
2020-10-28 17:27:13 +0300

    Compile with FastDDS precompiled binaryTarget

--- Package.swift
+++ Package.swift
@@ -2,8 +2,18 @@
 
 import PackageDescription
 
+#if os(Linux)
+let linkerSettings: [LinkerSetting]? = [
+    .linkedLibrary("fastrtps", .when(platforms: [.linux])),
+    .unsafeFlags(["-L/usr/local/lib"], .when(platforms: [.linux]))
+]
+#else
+let linkerSettings: [LinkerSetting]? = nil
+#endif
+
 let package = Package(
     name: "FastRTPSBridge",
+    platforms: [.iOS(.v11), .macOS(.v10_10)],
     products: [
         .library(
             name: "FastRTPSBridge",
@@ -15,15 +25,22 @@ let package = Package(
     targets: [
         .target(
             name: "FastRTPSWrapper",
-            path: "Sources/FastRTPSWrapper"),
+            dependencies: [
+                .target(name: "FastRTPS",
+                        condition: .when(platforms: [.iOS, .macOS, .watchOS]))
+                ],
+            path: "Sources/FastRTPSWrapper",
+            cxxSettings: [.define("FASTRTPS_FILTER")]),
+        .binaryTarget(name: "FastRTPS",
+                      url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/v1.0.0/fastrtps.xcframework.zip",
+                      checksum: "30df55cb2f793c5c78d66d7ea404fe24ffd4cc47e066cd09a6c99569c2641d7c"),
         .target(
             name: "FastRTPSBridge",
             dependencies: ["CDRCodable", "FastRTPSWrapper"],
             path: "Sources/FastRTPSBridge",
-            linkerSettings: [
-                .linkedLibrary("fastrtps"),
-                .unsafeFlags(["-L/usr/local/lib"])
-            ]
+            cxxSettings: [.define("FASTRTPS_FILTER")],
+            swiftSettings: [.define("FASTRTPS_FILTER")],
+            linkerSettings: linkerSettings
         ),
         .testTarget(
             name: "FastRTPSBridgeTests",
