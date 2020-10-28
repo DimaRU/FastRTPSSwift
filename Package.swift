124b01867aa12723ca0b0b71328682c68e59e2c8
Dmitriy Borovikov <dmitry@borovikov.name>
2020-10-29 00:46:42 +0300

    Package code formatting

--- Package.swift
+++ Package.swift
@@ -26,22 +26,22 @@ let package = Package(
         .package(name: "CDRCodable", url: "https://github.com/DimaRU/CDRCodable.git", from: "1.0.0"),
     ],
     targets: [
+        .binaryTarget(
+            name: "FastRTPS",
+            url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/v1.0.0/fastrtps.xcframework.zip",
+            checksum: "30df55cb2f793c5c78d66d7ea404fe24ffd4cc47e066cd09a6c99569c2641d7c"),
         .target(
             name: "FastRTPSWrapper",
             dependencies: dependencies,
             path: "Sources/FastRTPSWrapper",
             cxxSettings: [.define("FASTRTPS_FILTER")]),
-        .binaryTarget(name: "FastRTPS",
-                      url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/v1.0.0/fastrtps.xcframework.zip",
-                      checksum: "30df55cb2f793c5c78d66d7ea404fe24ffd4cc47e066cd09a6c99569c2641d7c"),
         .target(
             name: "FastRTPSBridge",
             dependencies: ["CDRCodable", "FastRTPSWrapper"],
             path: "Sources/FastRTPSBridge",
             cxxSettings: [.define("FASTRTPS_FILTER")],
             swiftSettings: [.define("FASTRTPS_FILTER")],
-            linkerSettings: linkerSettings
-        ),
+            linkerSettings: linkerSettings),
         .testTarget(
             name: "FastRTPSBridgeTests",
             dependencies: ["FastRTPSBridge"]),
