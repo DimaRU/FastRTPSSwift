51c4bd83a5c8c710e865278407b8dfab30f37a67
Dmitriy Borovikov <dmitry@borovikov.name>
2024-04-21 12:21:32 +0300

    WriterDiscoveryInfo and ReaderDiscoveryInfo documentation

--- Sources/FastRTPSSwift/ReaderDiscoveryInfo.swift
+++ Sources/FastRTPSSwift/ReaderDiscoveryInfo.swift
@@ -14,30 +14,36 @@ public struct ReaderDiscoveryInfo {
         self.info = info
     }
 
+    /// Return discovered topic name
     public var topicName: String {
         String(cString: info.pointee.topicName())
     }
     
+    /// Return discovered topic type
     public var typeName: String {
         String(cString: info.pointee.typeName())
     }
-
+    
+    /// Return discovered topic durability
     public var durability: Durability {
         Durability(rawValue: info.pointee.durability())!
     }
     
+    /// Return discovered topic
     public var reliability: Reliability {
         Reliability(rawValue: info.pointee.reliability())!
     }
     
+    /// Return true if discovered topic is keyed
     public var keyed: Bool {
         info.pointee.keyed()
     }
     
+    /// Return true if disabled positive ask on discovered topic
     public var disablePositiveACKs: Bool {
         info.pointee.disable_positive_acks()
     }
-    
+
     public var profile: RTPSWriterProfile {
         RTPSWriterProfile(
             keyed: keyed,
@@ -46,10 +52,12 @@ public struct ReaderDiscoveryInfo {
             disablePositiveACKs: disablePositiveACKs)
     }
 
+    /// Return remote unicast locators list
     public var unicastLocators: String {
         return String(info.pointee.getUnicastLocators())
     }
 
+    /// Return remote multicast locators list
     public var multicastLocators: String {
         return String(info.pointee.getMutlicastLocators())
     }
--- Sources/FastRTPSSwift/WriterDiscoveryInfo.swift
+++ Sources/FastRTPSSwift/WriterDiscoveryInfo.swift
@@ -14,26 +14,32 @@ public struct WriterDiscoveryInfo {
         self.info = info
     }
 
+    /// Return discovered topic name
     public var topicName: String {
         String(cString: info.pointee.topicName())
     }
     
+    /// Return discovered topic type
     public var typeName: String {
         String(cString: info.pointee.typeName())
     }
 
+    /// Return discovered topic durability
     public var durability: Durability {
         Durability(rawValue: info.pointee.durability())!
     }
     
+    /// Return discovered topic
     public var reliability: Reliability {
         Reliability(rawValue: info.pointee.reliability())!
     }
     
+    /// Return true if discovered topic is keyed
     public var keyed: Bool {
         info.pointee.keyed()
     }
     
+    /// Return true if disabled positive ask on discovered topic
     public var disablePositiveACKs: Bool {
         info.pointee.disable_positive_acks()
     }
@@ -45,10 +51,12 @@ public struct WriterDiscoveryInfo {
             durability: durability)
     }
     
+    /// Return remote unicast locators list
     public var unicastLocators: String {
         return String(info.pointee.getUnicastLocators())
     }
 
+    /// Return remote multicast locators list
     public var multicastLocators: String {
         return String(info.pointee.getMutlicastLocators())
     }
